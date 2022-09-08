a7cabbb831a27ea0b914b013f8fcdd67232deff0
Redi Hoxhaj <rhoxhaj@students.kennesaw.edu>
2022-09-08 19:42:13 -0400

    Pushing headerTabs

---
+++ components/HeaderTabs.js
@@ -0,0 +1,11 @@
+import react from "react";
+import { View, Text } from "react-native";
+
+
+export default function HeaderTabs(){ 
+      return (
+            <View>
+                  <Text>Header Tabs</Text>
+            </View>
+      );
+}
---
+++ screens/home.js
@@ -0,0 +1,12 @@
+import react from "react";
+import { View, Text, SafeAreaView } from "react-native";
+import HeaderTabs from "../components/HeaderTabs";
+
+
+export default function Home(){ 
+      return (
+            <SafeAreaView>
+                  <HeaderTabs/>
+            </SafeAreaView>
+      );
+}
