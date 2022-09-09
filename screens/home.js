b89411db02133d3d4690fb47b96a333933af8fce
Redi Hoxhaj <rhoxhaj@students.kennesaw.edu>
2022-09-08 22:14:39 -0400

    "Implemented Search bar feature and HeaderTab further

--- screens/home.js
+++ screens/home.js
@@ -1,12 +1,16 @@
 import react from "react";
 import { View, Text, SafeAreaView } from "react-native";
 import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
 
 
 export default function Home(){ 
       return (
-            <SafeAreaView>
-                  <HeaderTabs/>
+            <SafeAreaView style={{backgroundColor : "gray", flex: 1}}>
+                  <View style = {{backgroundColor : "white", padding: 20}}>
+                        <HeaderTabs />
+                        <SearchBar />
+                        </View>
             </SafeAreaView>
       );
 }
