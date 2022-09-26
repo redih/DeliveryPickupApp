32df25781a33c224d4136d90d04c43a2f3dd163e
Redi Hoxhaj <rhoxhaj@students.kennesaw.edu>
2022-09-26 12:41:08 -0400

    Categories component finished

--- components/Categories.js
+++ components/Categories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 
 
 const items = [
@@ -7,6 +7,10 @@ const items = [
 
             image: require("../assets/images/shopping-bag.png"),
             text: 'Pickup',
+      },
+      {
+            image: require("../assets/images/soft-drink.png"),
+            text: 'Soft Drinks',
       },
       {
             image: require("../assets/images/bread.png"),
@@ -28,34 +32,30 @@ const items = [
             image: require("../assets/images/desserts.png"),
             text: 'Desserts',
       },
-      {
-            image: require("../assets/images/soft-drink.png"),
-            text: 'Soft Drinks',
-      },
-      {
-            image: require("../assets/images/burger.png"),
-            text: 'Burger',
-      },
-      {
-            iamge: require("../assets/images/deals.png"),
-            text: 'Deals',
-      },
-      {
-            image: require("../assets/images/coffee.png"),
-            text: 'Coffee & Tea',
-      },
-      {
-            image: require("../assets/images/desserts.png"),
-            text: 'Desserts',
-      },
-
 ];
 
 
 export default function Categories() {
       return (
-            <View>
-                  <Image source = {items[0].image} />
+            <View style={{
+                  marginTop: 5,
+                  backgroundColor: "white",
+                  paddingVertical: 10,
+                  paddingLeft: 20,
+                  
+            }}>
+              <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                  {items.map((item, index) => (
+                   <View key={index} style = {{alignItems: "center", marginRight: 30}}>
+                     <Image source={item.image} style={{
+                       width: 50,
+                       height: 40,
+                       resizeMode: "contain",
+                       }} />
+                        <Text style={{ fontSize: 13, fontWeight: "900" }}>{item.text} </Text>
+                    </View>
+                   ))}
+                  </ScrollView>
             </View>
       );
 }
