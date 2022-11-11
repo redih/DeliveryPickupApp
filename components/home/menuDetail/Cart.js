20f34881870a5c570758f82d98720d8edd9747fa
Redi Hoxhaj <rhoxhaj@students.kennesaw.edu>
2022-11-11 18:13:08 -0500

    added ability to display total. Add or substract total.

--- components/home/menuDetail/Cart.js
+++ components/home/menuDetail/Cart.js
@@ -1,43 +1,57 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
-
+import { useSelector } from "react-redux";
 
 export default function Cart({}) {
+  const items = useSelector((state) => state.cartReducer.selectedItems.items);
+  const total = items
+    .map((item) => Number(item.price.replace("$", "")))
+    .reduce((a, b) => a + b, 0);
+
+  const totalAmount = total.toLocaleString("en", {
+    style: "currency",
+    currency: "USD",
+  });
+  console.log(totalAmount);
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "row",
-        position: "absolute",
-        bottom: 130,
-        zIndex: 999,
-      }}
-    >
+    <>
       <View
         style={{
-          flexDirection: "row",
+          flex: 1,
+          alignItems: "center",
           justifyContent: "center",
-          width: "100%",
+          flexDirection: "row",
+          position: "relative",
+          bottom: 5,
+          zIndex: 999,
         }}
       >
-        <TouchableOpacity
+        <View
           style={{
-            marginTop: 20,
-            backgroundColor: "#a89a32",
-            alignItems: "center",
-            // flexDirection: "row",
-            // justifyContent: "center",
-            padding: 15,
-            borderRadius: 50,
-            width: 200,
-            position: "relative",
+            flexDirection: "row",
+            justifyContent: "center",
+            width: "100%",
           }}
         >
-          <Text style={{ color: "black", fontSize: 15 }}> Cart </Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              marginTop: 20,
+              backgroundColor: "#a89a32",
+              flexDirection: "row",
+              justifyContent: "flex-end",
+              padding: 15,
+              borderRadius: 50,
+              width: 200,
+              position: "relative",
+            }}
+          >
+            <Text style={{ color: "black", fontSize: 20, marginRight: 50 }}>
+              Cart
+            </Text>
+            <Text style={{ color: "black", fontSize: 20 }}>{totalAmount}</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </>
   );
 }
--- components/home/menuDetail/MenuList.js
+++ components/home/menuDetail/MenuList.js
@@ -2,44 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { Divider } from "react-native-elements";
-import { useDispatch } from "react-redux";
-
-// const foodList = [
-//   {
-//     title: "Mexican Cups",
-//     description:
-//       "Mini Cups biscuits with seasoning mix, tomatoes and green chiles ",
-//     image:
-//       "https://images-gmi-pmc.edge-generalmills.com/d13cbaa5-fa4e-460c-ac53-9cdf02df562d.jpg",
-//     price: "4.00$",
-//   },
-//   {
-//     title: "Jalapeno Poppers Dip",
-//     description:
-//       "Cream cheese, jalapenos and bread crumbs are mixed to form a creamy dip",
-//     image:
-//       "https://hips.hearstapps.com/hmg-prod/images/190426-instant-pot-jalapeno-popper-dip-horizontal-089-1557946986.jpg",
-//     price: "5.50$",
-//   },
-//   {
-//     title: "Deep Fried Gucamole",
-//     description:
-//       "Bright guacamole is coated in crushed chips and deep-fried to golden perfection. ",
-//     image:
-//       "https://hips.hearstapps.com/hmg-prod/images/delish-deep-fried-guac-still005-1571070801.jpg?crop=0.561xw%3A0.561xh%3B0.192xw%2C0.222xh&resize=480%3A270",
-//     price: "4.50$",
-//   },
-//   {
-//     title: "Jicama Fries",
-//     description:
-//       "Jicama is a root vegetable that is a great substitute for potatoes. ",
-//     image:
-//       "https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/k%2FPhoto%2FRecipe%20Ramp%20Up%2F2022-01-Jicama-Fries%2FJicama_Fries_4",
-//     price: "3.50$",
-//   },
-
- 
-// ];
+import { useDispatch, useSelector } from "react-redux";
 
 const styles = StyleSheet.create({
   menuListStyle: {
@@ -54,16 +17,26 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function MenuList({ foodList}) {
+export default function MenuList({ foodList }) {
   //on press of checkbox the item will be added to the cart
   const dispatch = useDispatch();
   const selectItem = (item, checkboxValue) =>
     dispatch({
       type: "ADD_TO_CART",
-      payload: { ...item, checkboxValue: checkboxValue },
+      payload: {
+        ...item,
+        checkboxValue: checkboxValue,
+      },
     });
+
+  const cartItems = useSelector(
+    (state) => state.cartReducer.selectedItems.items
+  );
+  const isFoodInCart = (food, cartItems) =>
+    Boolean(cartItems.find((item) => item.title === food.title));
+
   return (
-    <ScrollView showsHorizontalScrollIndicator={false}>
+    <View>
       {foodList.map((food, index) => (
         <View key={index}>
           <View style={styles.menuListStyle}>
@@ -73,6 +46,7 @@ export default function MenuList({ foodList}) {
                 borderRadius: 7,
               }}
               fillColor="#a89a32"
+              isChecked={isFoodInCart(food, cartItems)}
               onPress={(checkboxValue) => selectItem(food, checkboxValue)}
             />
             <FoodInfo food={food} />
@@ -83,11 +57,9 @@ export default function MenuList({ foodList}) {
             orientation="vertical"
             style={{ marginHorizontal: 20 }}
           />
-          
-        
         </View>
       ))}
-    </ScrollView>
+    </View>
   );
 }
 
