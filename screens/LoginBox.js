040651c0fa3f812bde4f70114c4a43a13d20dfac
arman9511 <94088233+arman9511@users.noreply.github.com>
2022-12-01 18:52:20 -0500

    created an account page

--- screens/LoginBox.js
+++ screens/LoginBox.js
@@ -10,6 +10,7 @@ import {
   AppRegistry,
   KeyboardAvoidingView,
   StyleSheet,
+  Alert
 } from "react-native";
 import { auth } from "../firebase";
 import {
@@ -18,68 +19,13 @@ import {
 } from "firebase/auth";
 import { useEffect } from "react";
 
-// constructor(props) {
-//   super(props);
-//   this.state = {text: ''};
-// }
+
+
 
 const image =
   "https://media.discordapp.net/attachments/1011779060592033812/1014291034668290080/download_6.png";
 
-// const LoginBox = () => {
 
-//   const[email, setEmail]= useState('')
-//   const[pwd, setpwd]= useState('')
-
-//   return (
-//     <KeyboardAvoidingView
-//     style = {styles.container}
-//     behavior = "padding"
-//     >
-//       <View style = {styles.inputContainer}>
-//         <TextInput
-//           placeholder="Email"
-//           value={email}
-//           onChangeText = {text =>setEmail(text) }
-//           style = {styles.input}
-
-//         />
-
-//         <TextInput
-//           placeholder="PassWord"
-//           value={pwd}
-//           onChangeText = {text => setpwd(text)}
-//           style = {styles.input}
-//           secureTextEntry
-
-//         />
-//       </View>
-
-//       <View style = {styles.buttonContainer}>
-//         <TouchableOpacity
-//         onPress={() => { }}
-//         style = {styles.button}>
-//         <Text style = {styles.button}>Login
-//         </Text>
-//         </TouchableOpacity>
-
-//         <TouchableOpacity
-//         onPress={() => { }}
-//         style = {[styles.button, styles.buttonOutline]}>
-//         <Text style = {styles.buttonOutlineText}>Register
-//         </Text>
-//         </TouchableOpacity>
-//       </View>
-
-//       <Text>
-//         Login Screen
-//       </Text>
-
-//       </KeyboardAvoidingView>
-//   )
-
-// }
-// export default LoginBox
 
 export default function LoginBox({ navigation }) {
   const [email, setEmail] = useState("");
@@ -95,20 +41,14 @@ export default function LoginBox({ navigation }) {
   }, []);
 
   const handleSignUp = () => {
-    createUserWithEmailAndPassword(auth, email, pwd)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        console.log("Registered logged in with: ", user.email);
-      })
-      .catch((error) => alert(error.message));
+    navigation.navigate("CreateAccount");
   };
 
    const handleLoginin = () =>{
-    signInWithEmailAndPassword(auth, email, pwd)
+    signInWithEmailAndPassword(auth,email, pwd)
       .then((userCredential) => {
         const user = userCredential.user;
         console.log("Succesfully logged in with: ", user.email);
-
         navigation.navigate("Home");
       })
       .catch((error) => {
@@ -122,7 +62,7 @@ export default function LoginBox({ navigation }) {
       <ImageBackground
         alignItems="center"
         source={{ uri: image }}
-        blurRadius={1}
+        blurRadius={10}
         backgroundPosition="center"
         style={{ width: "100%", height: "100%" }}
       >
@@ -145,6 +85,7 @@ export default function LoginBox({ navigation }) {
 
           <TextInput
             style={{
+
               height: 45,
               width: "45%",
               borderColor: "black",
@@ -205,56 +146,3 @@ export default function LoginBox({ navigation }) {
   );
 }
 
-// const styles = StyleSheet.create({
-//   container:{
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center'
-
-//   },
-//   inputContainer:{
-//     width:"80%"
-
-//   },
-//   input:{
-//     backgroundColor: "white",
-//     paddingHorizontal:15,
-//     paddingVertical: 10,
-//     borderradius: 10,
-//     marginTop: 5,
-//   },
-//   buttonContainer:{
-//     width:"60%",
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     marginTop: 40,
-//   },
-//   button:{
-//     backgroundColor: '#0782F9',
-//     width:'100%',
-//     padding:15,
-//     borderRadius:10,
-//     alignItems:'center',
-
-//   },
-//   buttonOutline:{
-//     borderColor:'white',
-//     marginTop:5,
-//     borderColor: '#0782f9',
-//     borderWidth:2,
-
-//   },
-//   buttonText:{
-//     color:'white',
-//     fontWeight:'700',
-//     fontSize:16,
-
-//   },
-//   buttonOutlineText:{
-//     color:'#0782f9',
-//     fontWeight: '700',
-//     fontSize:16,
-
-//   }
-
-// })
